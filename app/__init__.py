0a84f45d3eed5c7f33e23a45f3a9f7076493db0d
Deron Nicholson <deron.nicholson@mymona.uwi.edu>
2019-04-23 04:22:30 +0000

    change

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = "./app/static/uploads"
 app.config['SECRET_KEY'] = "secret"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:password@localhost/project2"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://bnftelerivknuz:0f73dea9c0246000db210ace7f31f2321409691e4f9e014af91a2450aea3ab6c@ec2-54-197-234-117.compute-1.amazonaws.com:5432/d3qbffkqkggnbe"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
